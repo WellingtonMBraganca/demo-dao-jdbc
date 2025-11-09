f22ff701ed086a9a53d3212093080e9728e97eef
.Braganca <braganca.wellington@icloud.com>
2025-11-08 23:50:38 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        
+        Seller seller = new Seller(1, "jaja", "sadkjaklsj@kljfal", new Date(), 123.0, new Department(1, "green"));
+
+        System.out.println(seller);
     }
 }
